21df0d50d75105a99ea210e6dd7892436a0b7258
Vladimir Protcay <protcay2@mail.ru>
2022-06-15 23:48:48 +0300

    first

--- 7.2/jest.config.js
+++ 7.2/jest.config.js
@@ -2,4 +2,17 @@ module.exports = {
     verbose: true, //указывает на то, что каждый тест будет показан в отчете о процессе запуска 
      preset: "jest-puppeteer", //указываем, что будем использовать эту библиотеку
      testTimeout: 60000
+ }
+ {
+    "collectCoverageFrom": [
+        "**/*.{js,jsx}",
+        "!**/node_modules/**",
+        "!**/coverage/**"
+    ]
+ }
+ {
+    "coverageThreshold":
+        "branches": 100,
+        "functions": 100,
+        "lines": 100
  };
