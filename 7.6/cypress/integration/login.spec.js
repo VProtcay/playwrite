4be2e215d699bf7f7b6cf0dc1733e28d6c4efaac
Vladimir Protcay <protcay2@mail.ru>
2022-06-17 16:15:12 +0300

    dz6

--- 7.6/cypress/integration/login.spec.js
+++ 7.6/cypress/integration/login.spec.js
@@ -1,29 +1,27 @@
-it("Should successfully login", () => {
-  cy.visit("/booksNode");
+beforeEach(() => {
+  cy.visit("/");
+});
+
+it("Should open the main page", () => {
+  cy.contains("Books list");
+});
+
+it("Should succesfully login", () => {
   cy.login("test@test.com", "test");
-  cy.contains("Добро пожаловать test@test.com").should("be.visible");
+  cy.contains("Добро пожаловать").should("be.visible");
 });
 
 it("Should not login with empty login", () => {
-  cy.visit("/booksNode");
-  cy.contains("Log in").click();
-  cy.get("#mail").type(" ");
-  cy.get("#pass").type("test");
-  cy.contains("Submit").click();
-  cy.get("#mail")
-    .then(($el) => $el[0].checkValidity())
-    .should("be.false");
+  cy.login(" ", "test");
+  cy.checkIfElementInvalid("#mail");
   cy.get("#mail")
     .then(($el) => $el[0].validationMessage)
     .should("contain", "Please fill out this field.");
 });
 
 it("Should not login with empty password", () => {
-  cy.visit("/booksNode");
   cy.contains("Log in").click();
   cy.get("#mail").type("test@test.com");
   cy.contains("Submit").click();
-  cy.get("#pass")
-    .then(($el) => $el[0].checkValidity())
-    .should("be.false");
+  cy.checkIfElementInvalid("#pass");
 });
